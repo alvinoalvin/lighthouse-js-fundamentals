4531437941dd0c57f2837e4d8a2c3be2578ce529
alvinoalvin <alvinoalvin@gmail.com>
2021-03-09 21:09:41 -0800

    adding concat.js

---
+++ concat.js
@@ -0,0 +1,11 @@
+function concat(str1, str2) {
+  str2.forEach(str => {
+    str1.push(str)
+  })
+  return str1;
+}
+
+console.log(concat([1, 2, 3], [4, 5, 6]), "=?", [1, 2, 3, 4, 5, 6]);
+console.log(concat([0, 3, 1], [9, 7, 2]), "=?", [0, 3, 1, 9, 7, 2]);
+console.log(concat([], [9, 7, 2]), "=?", [9, 7, 2]);
+console.log(concat([5, 10], []), "=?", [5, 10]);
