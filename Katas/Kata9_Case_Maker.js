dfb08a5072354cbc88ff0984df0a64e85f3d05f4
alvinoalvin <alvinoalvin@gmail.com>
2021-03-22 17:12:22 -0700

    Kata9_Case_Matcher

---
+++ Katas/Kata9_Case_Maker.js
@@ -0,0 +1,12 @@
+const camelCase = function (input) {
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] == " ") {
+            input = input.substring(0, i) + input[i + 1].toUpperCase() + input.substring(i + 2, input.length)
+        }
+    }
+    return input
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
